e06319b8e3629e874a1169f110b5ee7bbea2465b
Eazy_Rydah <fabian.rhoda@gmx.net>
2019-08-27 12:15:35 +0000

    Initial Commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "cms");
+define("DB_USER", "cms_www");
+define("DB_PASS", "G7zQjqHfRFFd1qhc");
+
+define("SMTP_HOST", "s157.goserver.host");
+define("SMTP_USER", "web53p1");
+define("SMTP_PASS","ahd9Aegh");
+
+define("SHOW_ERROR_DETAIL", true);
